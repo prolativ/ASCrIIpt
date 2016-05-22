e64d84decc5836fad0310ee12470e82250bd3adc
mariusz89016 <mariusz89016@gmail.com>
2016-05-22 16:54:23 +0200

    Added JLine

--- build.sbt
+++ build.sbt
@@ -4,7 +4,12 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
+cancelable in Global := true
+
 libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
 libraryDependencies += "org.scalactic" %% "scalactic" % "2.2.6"
+libraryDependencies += "org.scalamock" %% "scalamock-scalatest-support" % "3.2.2" % "test"
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test"
 libraryDependencies += "com.googlecode.lanterna" % "lanterna" % "2.0.0"
+libraryDependencies += "jline" % "jline" % "2.12"
+
--- src/main/scala-2.11/ascriipt/animation/visualisation/FakeCanvas.scala
+++ src/main/scala-2.11/ascriipt/animation/visualisation/FakeCanvas.scala
@@ -2,6 +2,7 @@ package ascriipt.animation.visualisation
 
 import ascriipt.animation.model._
 import com.googlecode.lanterna.TerminalFacade
+import jline.console.ConsoleReader
 
 class FakeCanvas(width: Int, height: Int) extends Canvas {
   var canvas: Array[Array[Char]] = Array()
@@ -9,122 +10,72 @@ class FakeCanvas(width: Int, height: Int) extends Canvas {
   screen.startScreen()
   clear()
 
-  new Thread(new Runnable {
-    override def run(): Unit = {
-      while (screen.readInput() == null) {
-        Thread.sleep(100)
-      }
-      System.exit(1)
-    }
-  }).start()
-
-  override def clear(): Unit = canvas = Array.fill(height, width)('.')
+  override def clear(): Unit = canvas = Array.fill(height, width)(' ')
 
   override def drawChar(startingPoint: (Int, Int), char: Char): Unit = {
     val (x, y) = startingPoint
-    if (y < height && x < width)
+    if(x >= 0 && x < width && y >= 0 && y < height) {
       canvas(y)(x) = char
+    }
   }
 
   override def show(): Unit = {
     val terminal = screen.getTerminal
-    for (x <- 0 until width) {
-      for (y <- 0 until height) {
-        terminal.moveCursor(x, y)
-        terminal.putCharacter(canvas(y)(x))
-      }
+
+    for {
+      x <- 0 until width
+      y <- 0 until height
+    } {
+      terminal.moveCursor(y, x)
+      terminal.putCharacter(canvas(y)(x))
     }
+
+  }
+
+  def close(): Unit = {
+    screen.stopScreen()
   }
 }
 
 object FakeCanvas {
-  def main(args: Array[String]) {
-    val image = Array(
-      Array('\0', '\0', '-', '-', '\0', '\0'),
-      Array('\0', '/', '\0', '\0', '\\', '\0'),
-      Array('|', '\0', '\0', '\0', '\0', '|'),
-      Array('|', '\0', '\0', '\0', '\0', '|'),
-      Array('\0', '\\', '\0', '\0', '/', '\0'),
-      Array('\0', '\0', '-', '-', '\0', '\0')
-    )
-//    val sequence = ParallelAnimation(Seq(
-//      TimedWaiting(10),
-//      AsciiPointFixedDistanceMovement(0, 0, 4, 4, 'O'),
-//      AsciiPointFixedDistanceMovement(8, 8, 0, 0, 'X'),
-//      AsciiPointFixedDistanceMovement(1, 2, 3, 7, 'A'),
-//      AsciiImageMovement(4, 4, 10, 10, '.', new AsciiImage(image))
-//    ))
+  def main(args: Array[String]): Unit = {
+    val c: ConsoleReader = new ConsoleReader(System.in, System.out)
+    while(true) {
+      c.println("asvdsfas")
+      c.println("asvdsfas")
+      c.println("asvdsfas")
+      c.flush()
+      Thread.sleep(500)
+      c.clearScreen()
+      c.flush()
+      Thread.sleep(500)
 
-    val sequence = SequentialAnimation(Seq(
-      TimedWaiting(6),
-      TimedWaiting(3),
-      ParallelAnimation(Seq(
-        TimedWaiting(4),
-        AsciiPointFixedDistanceMovement(10, 10, 4, 4, 'O')
-      )),
-      TimedWaiting(3),
-      ParallelAnimation(Seq(
-        TimedWaiting(4),
-        AsciiPointFixedDistanceMovement(14, 14, 10, 10, 'O')
-      ))
-    ))
+      c.println("zxcasd")
+      c.println("!@!@#")
+      c.flush()
+      Thread.sleep(500)
+    }
 
-    val seq1 = ParallelAnimation(Seq(
-      TimedWaiting(20),
-      SequentialAnimation(Seq(
-        AsciiPointFixedDistanceMovement(4, 4, 0, 0, 'A')
-      ))
-    ))
-
-    val seq2 = ParallelAnimation(Seq(
-      TimedWaiting(20),
-      SequentialAnimation(Seq(
-        ParallelAnimation(Seq(
-          TimedWaiting(7),
-          AsciiPointFixedDistanceMovement(0, 0, 4, 4, 'X')
-        )),
-        UntimedWaiting,
-        ParallelAnimation(Seq(
-          TimedWaiting(7),
-          AsciiPointFixedDistanceMovement(4, 4, 0, 0, '^')
-        ))
-      ))
-    ))
-
-    val seq3 = ParallelAnimation(Seq(
-      TimedWaiting(20),
-      AsciiImageMovement(0, 0, 8, 20, '.', new AsciiImage(image))
-//      AsciiImageMovement(20, 0, 0, 20, '.', new AsciiImage(image)),
-//      AsciiImageMovement(0, 54, 8, 34, '.', new AsciiImage(image)),
-//      AsciiImageMovement(20, 54, 0, 34, '.', new AsciiImage(image))
-
-    ))
-
-
-//    drawAnimation(sequence)
-//    drawAnimation(seq1)
-//    drawAnimation(seq2)
-    drawAnimation(ParallelAnimation(Seq(
-      sequence, seq1, seq2, seq3
-    )))
   }
 
   def drawAnimation(animation: Animation): Unit = {
     implicit val canvas = new FakeCanvas(60, 25)
 
-    while (true) {
+    try {
       animation.baseDuration match {
         case ExactDuration(totalDuration) => {
-          val timeStep = 1
+          val timeStep = 100
           (0L until totalDuration by timeStep).foreach { atTime =>
             animation.draw(atTime, totalDuration)
             canvas.show()
-            Thread.sleep(200)
+            Thread.sleep(timeStep)
             canvas.clear()
           }
         }
         case _ => throw AnimationTimeException("Root animation has to have exact duration given.")
       }
+    } finally {
+      canvas.close()
     }
   }
 }
